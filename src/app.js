0e07a47c0be0e19ea0b8508be8c78ca36081c5fc
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-01-28 08:37:48 +0530

    Handling multiple route handlers

--- src/app.js
+++ src/app.js
@@ -3,14 +3,22 @@ const express = require('express');
 const app = express();
 
 
-//This will only handle get call to user
-app.get("/user/:userID/:name",(req,res) =>{
-    console.log(req.params);
-    // console.log(req.query);
-
-    res.send({firstName:"Himasnhu", lastname:"Yadav"});
-});
-
+app.use("/user",(req,res,next)=>{
+    console.log("Handling the route user1..")
+   // res.send("1st Response...");
+    next();
+},
+    (req,res,next)=>{
+            console.log("Handling the route user2..")
+       // res.send("2nd Response....");
+        next();
+    },
+     (req,res,next)=>{
+            console.log("Handling the route user3..")
+        res.send("3rd Response....");
+       // next(); here it will give error because there is no next so the user call will not be able to find any response
+    }
+);
 app.listen(911 , () =>{
     console.log("Server is successfully listening to our port 911...");
 });
