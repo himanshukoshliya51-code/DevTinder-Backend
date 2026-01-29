1ac777bf4ef852f50da84ee111ba050f6c1653e4
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-01-29 08:07:44 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -2,22 +2,41 @@ const express = require('express');
 
 const app = express();
 
-//Another way to use multiple route handlers
-//works same way as previous
-app.get("/user",(req,res,next)=>{
-    console.log("Handling the route user1..")
-   // res.send("1st Response...");
-    next(); // if we donot have next here and res.send then the postman will load foreever the user will not get the response of its call
-    //here this function is called middleware
+const {adminAuth,userAuth}= require('./middlewares/auth');
+
+
+//Handle auth Middleware for all request(GET,POST,.....)
+app.use("/admin",adminAuth);
+
+app.get("/admin/getAllData",(req,res) =>{
+    res.send("All data sent");
 });
 
-app.get("/user",(req,res,next)=>{
-    console.log("Handling the route user2..")
-    res.send("2nd Response...");
-    //here this function is called route handler
-    // next(); here also next will give error in postman 
-    
+app.get("/admin/deleteUser",(req,res) =>{
+    res.send("All data Deleted");
 });
+
+app.get("/user",userAuth,(req,res) =>{
+    res.send("All data Deleted");
+});
+
+
+// //Another way to use multiple route handlers
+// //works same way as previous
+// app.get("/user",(req,res,next)=>{
+//     console.log("Handling the route user1..")
+//    // res.send("1st Response...");
+//     next(); // if we donot have next here and res.send then the postman will load foreever the user will not get the response of its call
+//     //here this function is called middleware
+// });
+
+// app.get("/user",(req,res,next)=>{
+//     console.log("Handling the route user2..")
+//     res.send("2nd Response...");
+//     //here this function is called route handler
+//     // next(); here also next will give error in postman 
+    
+// });
 
 
 app.listen(911 , () =>{
