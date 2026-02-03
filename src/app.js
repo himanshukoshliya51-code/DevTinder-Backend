157922d56c94747d2b6d95e0f8c3fa12afa77f69
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-02-03 20:59:52 +0530

    init - database

--- src/app.js
+++ src/app.js
@@ -1,35 +1,32 @@
-const express = require('express');
-
+const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-//Error handling......
-app.get("/getUserdata",(req,res) =>{
-//try catch is proper way it should be used always, but what if there are some unhandled errors
+app.post("/signup", async (req, res) => {
+  // Creating a new instance of the User model
+  const user = new User({
+    firstName: "Sachin",
+    lastName: "Tendulkar",
+    emailId: "sachin@kohli.com",
+    password: "sachin@123",
+  });
 
-// try{
-    throw new error("njvdjv");
-    res.send("User data sent");
-// }
-// catch(err){
-// res.status(500).send("some error:.......")
-// }
-//here we have catched error here only so it will not go to the next "/" functon because that function require some error
+  try {
+    await user.save();
+    res.send("User Added successfully!");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
 });
 
-
-
-
-//this function will not be called first even if "/" because it has err ,it will only be runned if err present
-//this function will only be called when we have some error otherwise it will not be runned
-app.use("/",(err,req,res,next)=>{//error should be 1st parameter
-if(err){
-    res.status(500).send("Something went wrong");
-}
-});
-//this function should be kept at last of our application  because if anything breaks in our code it should be handled here 
-
-
-
-app.listen(911 , () =>{
-    console.log("Server is successfully listening to our port 911...");
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
