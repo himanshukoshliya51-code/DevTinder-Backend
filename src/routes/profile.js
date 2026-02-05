cb0ec443bf1fcf06da25d99b5efc11430aad336f
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-02-05 20:04:42 +0530

    logout+edit profile API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const {validateEditProfileData} = require("../utils/validation")
 
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
 
@@ -12,5 +13,27 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
     res.status(400).send("ERROR : " + err.message);
   }
 });
+
+profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
+    try{
+        if(!validateEditProfileData(req)){
+            throw new Error("Invalid Edit request");
+            // return res.status(400).send("")
+        }
+
+        const loggedInUser = req.user;
+        
+        Object.keys(req.body).forEach((key)=>loggedInUser[key] = req.body[key]);
+        await loggedInUser.save();
+       
+        res.json({message:`${loggedInUser.firstName},your profile updated successfullyyy`,
+            data:loggedInUser,
+        });     
+    }
+    catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+
+})
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -18,6 +18,16 @@ const validateSignUpData = (req) => {
     }
 };
 
+const validateEditProfileData = (req) => {
+    const allowedEditFields = ["firstName","lastName","photoUrl","gender","age","about","skills",];
+
+    const isEditAllowed = Object.keys(req.body).every((field)=>allowedEditFields.includes(field));
+    return isEditAllowed;
+
+
+};
+
 module.exports = {
     validateSignUpData,
+    validateEditProfileData,
 };
