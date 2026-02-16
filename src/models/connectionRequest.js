bd14c9b71059aab006789fbb31296d189e86286f
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-02-16 22:18:54 +0530

    Fetching connections API

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,15 +1,15 @@
-
 const mongoose = require("mongoose");
 
 const connectionRequestSchema = new mongoose.Schema(
   {
     fromUserId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User", // refernce to the user collection
+      ref: "User",
       required: true,
     },
     toUserId: {
       type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
     },
     status: {
