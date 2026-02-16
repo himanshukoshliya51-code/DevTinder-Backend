7b064639ea2dd437d497c703fde42c5b444fe8e9
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-02-16 19:34:30 +0530

    API connection request accept/reject

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -14,7 +14,7 @@ const connectionRequestSchema = new mongoose.Schema(
       type: String,
       required: true,
       enum: {
-        values: ["ignored", "interested", "accepeted", "rejected"],
+        values: ["ignored", "interested", "accepted", "rejected"],
         message: `{VALUE} is incorrect status type`,
       },
     },
@@ -22,18 +22,18 @@ const connectionRequestSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// ConnectionRequest.find({fromUserId: 273478465864786587, toUserId: 273478465864786587})
+ //ConnectionRequest.find({fromUserId: 273478465864786587, toUserId: 273478465864786587})
 
 connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
-connectionRequestSchema.pre("save", function (next) {
-  const connectionRequest = this;
-  // Check if the fromUserId is same as toUserId
-  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
-    throw new Error("Cannot send connection request to yourself!");
-  }
-  next();
-});
+// connectionRequestSchema.pre("save", function (next) {
+//   const connectionRequest = this;
+//   // Check if the fromUserId is same as toUserId
+//   if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+//     throw new Error("Cannot send connection request to yourself!");
+//   }
+//   next();
+// });
 
 const ConnectionRequestModel = new mongoose.model(
   "ConnectionRequest",
