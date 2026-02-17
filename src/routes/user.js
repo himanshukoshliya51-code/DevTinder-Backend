d720c8ad15121641b07eb9967bf66c76bb4e1d14
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-02-17 07:35:58 +0530

    feed API + pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -58,25 +58,25 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+
     const page = parseInt(req.query.page) || 1;
     let limit = parseInt(req.query.limit) || 10;
     limit = limit > 50 ? 50 : limit;
-
     const skip = (page - 1) * limit;
 
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
-    }).select("fromUserId toUserId");
+    }).select("fromUserId  toUserId");
 
-    const hideUserFromFeed = new Set();
+    const hideUsersFromFeed = new Set();
     connectionRequests.forEach((req) => {
-      hideUserFromFeed.add(req.fromUserId.toString());
-      hideUserFromFeed.add(req.toUserId.toString());
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
     });
 
     const users = await User.find({
       $and: [
-        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
     })
@@ -84,7 +84,7 @@ userRouter.get("/feed", userAuth, async (req, res) => {
       .skip(skip)
       .limit(limit);
 
-    res.send(users);
+    res.json({ data: users });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
